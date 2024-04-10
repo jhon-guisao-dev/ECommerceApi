7cea248713c5ee4619a7e24cdd4c25dc38cb3d0e
jhon-guisao-dev <jhonguisao.dev@gmail.com>
2024-04-10 08:14:14 -0500

    Agregar archivos de proyecto.

---
+++ ECommerceApi/Mutations/ProductQuery.cs
@@ -0,0 +1,16 @@
+﻿namespace ECommerceApi.Mutations
+{
+    public class ProductQuery
+    {
+        [GraphQLName("products")]
+        public IEnumerable<String> GetProducts()
+        {
+            return new List<String>
+            {
+                "Product 1",
+                "Product 2",
+                "Product 3"
+             };
+        }
+    }
+}
